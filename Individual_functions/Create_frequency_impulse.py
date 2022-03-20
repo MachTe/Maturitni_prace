452730eeaf715dc0b54dc3f94488d215f3b717f4
MachTe <75760102+MachTe@users.noreply.github.com>
2022-03-20 21:32:02 +0100

    Update Create_frequency_impulse.py
    Formally introducing delta

--- Individual_functions/Create_frequency_impulse.py
+++ Individual_functions/Create_frequency_impulse.py
@@ -1,7 +1,7 @@
 import math
 
 
-def get_frequency_impulse(frequency, sample_num, sample_rate=44100, convolution_magnitude=5):
+def get_frequency_impulse(frequency, sample_num, sample_rate=44100, convolution_magnitude=4, delta=0.07):
     # Creates an impulse of a given frequency. The edges are smoothed by convolution.
 
     # frequency: positive integer, interval (20; 20000) can be heard by a human.
@@ -9,13 +9,12 @@ def get_frequency_impulse(frequency, sample_num, sample_rate=44100, convolution_
     # sample_rate: positive integer, has to be at least two times larger than frequency!
     # convolution_magnitude: positive number, advised interval (1; 10) The smaller the smoother!
 
-    negligible = math.log(1/0.002)**(1 / convolution_magnitude)
-    # Sets x coordinate at which Gaussian function becomes negligible.
-    # 0.002 being the cutoff magnitude. (Anything smaller is considered negligible.)
+    # delta: the cutoff magnitude of Gaussian function, at which is becomes negligible.
     
     data = []
     for sample in range(sample_num):
-        data.append(math.sin(2*math.pi * frequency * sample / sample_rate) * math.e ** (-abs(negligible*(2*sample/sample_num - 1)) ** (convolution_magnitude)))
+        data.append(round(32767 * amplitude * math.sin(2 * math.pi * frequency * sample / sample_rate) * delta ** (
+            abs((sample * 2 / sample_num - 1)) ** convolution_magnitude), 0))
     # This loop samples desired sine wave convoluted by Gaussian function.
     
     return data
