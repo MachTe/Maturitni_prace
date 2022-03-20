462025fb5608e58eb68d84da7e51e56ee1918baf
MachTe <75760102+MachTe@users.noreply.github.com>
2022-03-20 21:39:50 +0100

    Update Realtime_listening.py
    Aligning the wording with the one in the pdf file

--- Individual_functions/Realtime_listening.py
+++ Individual_functions/Realtime_listening.py
@@ -2,20 +2,20 @@ import sounddevice as sd
 import math
 
 default_samplerate = 44100
-target_frequency = 21000
-buffer_lenght = 147
+carrier_frequency = 20700
+impulse_lenght = 49
 
 def audio_callback(indata, frames, time, status):
-  # Listens to a buffer_lenght of samples and then looks for the target frequency.
+  # Listens to a impulse_lenght of samples and then looks for the target frequency.
   
    Sigma = 0
-   freq = frames / default_samplerate * target_frequency
+   freq = frames / default_samplerate * carrier_frequency
    for sample in range(len(indata)):
       Sigma += indata[sample][0] * math.e ** (-2 * freq * math.pi * sample / len(indata) * 1j)
-   print(str(abs(Sigma)) + " is the amplitude of " + str(target_frequency) + "Hz frequency.")
+   print(str(abs(Sigma)) + " is the amplitude of " + str(carrier_frequency) + "Hz frequency.")
 
 
-stream = sd.InputStream(callback=audio_callback, samplerate=default_samplerate, blocksize=buffer_lenght)
+stream = sd.InputStream(callback=audio_callback, samplerate=default_samplerate, blocksize=impulse_lenght)
 
 with stream:
   # Allows the listening to last for 1 second.
