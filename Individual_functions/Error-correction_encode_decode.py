bcaf32d86f30608877e8a525845d2b9ef2c89c91
MachTe <75760102+MachTe@users.noreply.github.com>
2022-03-20 21:36:31 +0100

    Update Error-correction_encode_decode.py
    Renaming variables to match the wording in the pdf file

--- Individual_functions/Error-correction_encode_decode.py
+++ Individual_functions/Error-correction_encode_decode.py
@@ -5,10 +5,10 @@ def get_encoding(data, backup_bytes, enc='IBM852'):
     '''Uses Reed-Solomon error-correction code to transform a String into ones and zeros which are to be send.
 
     # data: String of IBM852 characters
-    # backup_bytes: Number of bytes protected against flipping
-    # bytes_as_bits: String of ones and zeros'''
+    # error_correction_symbols: Number of bytes protected against flipping
+    # data_as_bits: String of ones and zeros'''
 
-    rsc = RSCodec(2 * backup_bytes)
+    rsc = RSCodec(2 * error_correction_symbols)
 
     if type(data) == type(2):
         st = (16 - len(str(bin(data))[2:])) * "0" + str(bin(data))[2:]
@@ -18,14 +18,14 @@ def get_encoding(data, backup_bytes, enc='IBM852'):
         a = rsc.encode(bytes(data, enc, 'replace'))
         # unknown characters are replaced with a '?'
 
-    bytes_as_bits = ''.join(format(byte, '08b') for byte in a)
-    return bytes_as_bits
+    data_as_bits = ''.join(format(byte, '08b') for byte in a)
+    return data_as_bits
 
 
-def get_decoding(data, backup_bytes, enc='IBM852'):
+def get_decoding(data, error_correction_bytes, enc='IBM852'):
     '''Decodes the String of ones and zeros produced by the get_encoding() back into the initial message.'''
 
-    rsc = RSCodec(2 * backup_bytes)
+    rsc = RSCodec(2 * error_correction_bytes)
     message = rsc.decode(bytearray([(int(data[i:i + 8], 2)) for i in range(0, len(data), 8)]))[0].decode(encoding=enc)
     # Cuts data into segments of 8 bits and translates them back into characters
 
