17784936c1569d7d8191bc8fa89b9b674b4da61e
MachTe <75760102+MachTe@users.noreply.github.com>
2022-03-20 21:54:25 +0100

    Update transmitting_program.py
    finishing touches and a removal of a vulgar word

--- main/transmitting_program.py
+++ main/transmitting_program.py
@@ -28,55 +28,51 @@ def save_wav(file_name, audio_data, sample_rate=44100):
     return
 
 
-def get_frequency_impulse(frequency, sample_num, amplitude=0.75, sample_rate=44100, convolution_magnitude=2):
-    '''Creates an impulse of a given frequency. The edges are smoothed by convolution.
+def get_frequency_impulse(frequency, impulse_length, sample_rate=44100, convolution_magnitude=4, delta=0.07):
+    # Creates an impulse of a given frequency. The edges are smoothed by convolution.
 
     # frequency: positive integer, interval (20; 20000) can be heard by a human.
-    # sample_num: number of created samples, equal to len(data)
-    # amplitude: float, 1 being loudest possible, 0 being silence.
+    # impulse_length: number of created samples, equal to len(data)
     # sample_rate: positive integer, has to be at least two times larger than frequency!
     # convolution_magnitude: positive number, advised interval (1; 10) The smaller the smoother!
 
-    # negligible: float, sets x coordinate at which Gaussian function becomes negligible.
-    # 0.0001 being the cutoff magnitude. (Anything smaller is considered negligible.)'''
-
-    negligible = math.log(1 / 0.0001) ** (1 / convolution_magnitude)
+    # delta: the cutoff magnitude of Gaussian function, at which is becomes negligible.
+    
     data = []
-
-    for sample in range(sample_num):
-        # This loop samples desired sine wave convoluted by Gaussian function.
-        data.append(round(32767 * amplitude * math.sin(2 * math.pi * frequency * sample / sample_rate) * math.e ** (
-            -abs(negligible * (2 * sample / sample_num - 1)) ** convolution_magnitude), 0))
-
+    for sample in range(impulse_length):
+        data.append(round(32767 * amplitude * math.sin(2 * math.pi * frequency * sample / sample_rate) * delta ** (
+            abs((sample * 2 / impulse_length - 1)) ** convolution_magnitude), 0))
+    # This loop samples desired sine wave convoluted by Gaussian function.
+    
     return data
 
 
-def get_encoding(data, backup_bytes, enc='IBM852'):
+def get_encoding(data, error_correction_symbols, enc='IBM852'):
     '''Uses Reed-Solomon error-correction code to transform a String into ones and zeros which are to be send.
 
-    # data: String of ASCII characters
-    # backup_bytes: Number of bits protected against flipping
-    # bytes_as_bits: String of ones and zeros'''
+    # data: String of IBM852 characters
+    # error_correction_symbols: Number of bytes protected against flipping
+    # data_as_bits: String of ones and zeros'''
 
-    rsc = RSCodec(2 * backup_bytes)
+    rsc = RSCodec(2 * error_correction_symbols)
 
     if type(data) == type(2):
-        # not wasting space for binary numbers
         st = (16 - len(str(bin(data))[2:])) * "0" + str(bin(data))[2:]
         a = rsc.encode(bytearray([int(st[:8], 2), int(st[8:], 2)]))
+        # not wasting space for binary numbers
     else:
-        # unknown characters are replaced with a '?'
         a = rsc.encode(bytes(data, enc, 'replace'))
+        # unknown characters are replaced with a '?'
 
-    bytes_as_bits = ''.join(format(byte, '08b') for byte in a)
-    return bytes_as_bits
+    data_as_bits = ''.join(format(byte, '08b') for byte in a)
+    return data_as_bits
 
 
-def Transmit(frequency, chunk_size, convolution_magnitude=2, amplitude=0.75, sample_rate=44100, cushion=30, enc='IBM852'):
+def Transmit(frequency, chunk_size, convolution_magnitude=4, delta=0.07, amplitude=0.75, sample_rate=44100, cushion=30, enc='IBM852'):
     '''THE MAIN FUNCTION used to transmit messages via sound
 
     # frequency: integer: between 1 and sample_rate/2, frequency on which the data is transmitted
-    # chunk_size: integer: length of a frequency impulse in samples.
+    # impulse_length: integer: length of a frequency impulse in samples.
     # convolution_magnitude: positive number, advised interval (1; 10) The smaller the smoother!
     # amplitude: float, 1 being loudest possible, 0 being silence.
     # sample_rate: positive integer, has to be at least two times larger than frequency!
@@ -87,7 +83,7 @@ def Transmit(frequency, chunk_size, convolution_magnitude=2, amplitude=0.75, sam
 
     while True:
         audio_data = []
-        binary_data = "111111111111111111111111111111111111111111111111111111111111111111111111100000000000000000000000"
+        binary_data = "1111111111111111111111111111111111111111"
         message = input("Enter your message: ")
 
         binary_data += get_encoding("Đ", 1, enc=enc)
@@ -97,8 +93,8 @@ def Transmit(frequency, chunk_size, convolution_magnitude=2, amplitude=0.75, sam
         binary_data += get_encoding(message, cushion, enc=enc)
         # the message itself and its cushion.
 
-        impulse = get_frequency_impulse(frequency, chunk_size, convolution_magnitude=convolution_magnitude, amplitude=amplitude, sample_rate=sample_rate)
-        silence = [0.0] * chunk_size
+        impulse = get_frequency_impulse(frequency, impulse_length=impulse_length, convolution_magnitude=convolution_magnitude, delta=delta, amplitude=amplitude, sample_rate=sample_rate)
+        silence = [0.0] * impulse_length
 
         for i in binary_data:
             if i == "1":
@@ -109,10 +105,10 @@ def Transmit(frequency, chunk_size, convolution_magnitude=2, amplitude=0.75, sam
         audio_data += [0.0]*800
         # Adds some silence at the end of the audio as abrupt end sometimes causes cracking in the speaker.
 
-        save_wav("hovno.wav", audio_data, sample_rate=sample_rate)
+        save_wav("message.wav", audio_data, sample_rate=sample_rate)
 
-        p = vlc.MediaPlayer("hovno.wav")
+        p = vlc.MediaPlayer("message.wav")
         p.play()
 
 
-Transmit(20700, 49, convolution_magnitude=2, amplitude=0.75, sample_rate=44100, cushion=30, enc='IBM852')
+Transmit(20700, 49, convolution_magnitude=4, delta=0.07, amplitude=0.75, sample_rate=44100, cushion=30, enc='IBM852')
